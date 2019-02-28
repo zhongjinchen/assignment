5443f4b24508e68741df825b067acb5b65a7f70e
自由之翼 <@LAPTOP-0QVJ42G4>
2019-02-28 14:48:46 +0800

    change

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -9,12 +9,25 @@ namespace assignment
             Console.WriteLine("Hello World!");
             // var span = DateTime.Now - new DateTime(2019,1,1);
             //Console.WriteLine(span);
-            DateTime date = new DateTime(2019, 1, 6);
-            for (int week = 1; week < 52; week++)
+            DateTime date = new DateTime(2019, 1, 1);
+            for (int day = 1; day < 7; day++)
             {
-                DateTime Todate = date.AddDays(7 * week - 6);
+                if (date.DayOfWeek == DayOfWeek.Monday)
+                {
+                    break;
+                }
+                else
+                {
+                    date = date.AddDays(1);
+                }
+            }
+            Console.WriteLine($"{date.Year}年{date.Month}月{date.Day}日为该年第一个星期一");
+            
+            for (int week = 1; week < 53; week++)
+            {
+                DateTime Todate = date.AddDays(7 * week-7);
                 Console.Write($"第{week}周:{Todate.Year}年{Todate.Month}月{Todate.Day}日-");
-                DateTime Expirydate = date.AddDays(7 * week);
+                DateTime Expirydate = date.AddDays(7 * week-1);
                 Console.WriteLine($"{Expirydate.Year}年{Expirydate.Month}月{Expirydate.Day}日");
                 Console.WriteLine();
             }
