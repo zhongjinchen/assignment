111c6730770a21484ba0f46e90756af2c4796a19
自由之翼 <@LAPTOP-0QVJ42G4>
2019-03-01 17:23:10 +0800

    revise

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -22,14 +22,46 @@ namespace assignment
                 }
             }
             Console.WriteLine($"{date.Year}年{date.Month}月{date.Day}日为该年第一个星期一");
-            
-            for (int week = 1; week < 53; week++)
+            //int i = 0;
+            //DateTime secondyear = date;
+            //bool nextYear = false;
+            //while (!nextYear)
+            //{
+            //    //i++;
+            //    //if(secondyear.Year > 2019)
+            //    //{
+            //    //    success = true;
+            //    //    secondyear = date.AddDays(7*(i+1));
+
+            //    //}
+            //    //else
+            //    //{
+            //    //    success = false;
+            //    //}
+
+            //    date.AddDays(7);
+            //    nextYear = date.Year > 2019;                
+            //}
+
+            //Console.WriteLine($"i={i}");
+            int week = 1;
+            bool success = false;
+            DateTime expiryDate = date;
+            while(!success)                          //for (int week = 1;week<4; week++)
             {
-                DateTime Todate = date.AddDays(7 * week-7);
-                Console.Write($"第{week}周:{Todate.Year}年{Todate.Month}月{Todate.Day}日-");
-                DateTime Expirydate = date.AddDays(7 * week-1);
-                Console.WriteLine($"{Expirydate.Year}年{Expirydate.Month}月{Expirydate.Day}日");
-                Console.WriteLine();
+                if(expiryDate.Year <2020)
+                {
+                    DateTime Todate = date.AddDays((7 * week) - 7);
+                    Console.Write($"第{week}周:{Todate.Year}年{Todate.Month}月{Todate.Day}日-");
+                    expiryDate = date.AddDays(7 * week - 1);
+                    Console.WriteLine($"{expiryDate.Year}年{expiryDate.Month}月{expiryDate.Day}日");
+                    Console.WriteLine();
+                    week++;
+                }
+                else
+                {
+                    success = false;
+                }
             }
 
 
