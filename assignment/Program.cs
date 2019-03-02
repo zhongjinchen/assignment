ed50dfe2d8c545bdaffae43c27d56c125dec2f65
自由之翼 <@LAPTOP-0QVJ42G4>
2019-03-02 22:49:25 +0800

    change

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -6,9 +6,16 @@ namespace assignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            // var span = DateTime.Now - new DateTime(2019,1,1);
-            //Console.WriteLine(span);
+            Mimicstact imitate = new Mimicstact(5);
+            imitate.Push(8);
+            imitate.Push(10);
+            imitate.Push(4);
+            imitate.Push(7);
+            imitate.Push(5);
+            Console.Read();
+        }
+        static void Date()
+        {
             DateTime date = new DateTime(2019, 1, 1);
             for (int day = 1; day < 7; day++)
             {
@@ -22,34 +29,14 @@ namespace assignment
                 }
             }
             Console.WriteLine($"{date.Year}年{date.Month}月{date.Day}日为该年第一个星期一");
-            //int i = 0;
-            //DateTime secondyear = date;
-            //bool nextYear = false;
-            //while (!nextYear)
-            //{
-            //    //i++;
-            //    //if(secondyear.Year > 2019)
-            //    //{
-            //    //    success = true;
-            //    //    secondyear = date.AddDays(7*(i+1));
 
-            //    //}
-            //    //else
-            //    //{
-            //    //    success = false;
-            //    //}
-
-            //    date.AddDays(7);
-            //    nextYear = date.Year > 2019;                
-            //}
-
-            //Console.WriteLine($"i={i}");
             int week = 1;
             bool success = false;
+
             DateTime expiryDate = date;
-            while(!success)                          //for (int week = 1;week<4; week++)
+            while (!success)
             {
-                if(expiryDate.Year <2020)
+                if (expiryDate.Year < 2020)
                 {
                     DateTime Todate = date.AddDays((7 * week) - 7);
                     Console.Write($"第{week}周:{Todate.Year}年{Todate.Month}月{Todate.Day}日-");
@@ -63,11 +50,62 @@ namespace assignment
                     success = false;
                 }
             }
+        }
+    }
+    class Mimicstact
+    {
+        private int[] array = null;
+        private int top;
+        private int max;
+        public Mimicstact(int Length)
+        {
+            array = new int[Length];
+        }
+        internal void Push(int[]value)
+        {
+            for (int i = 0; i <value.Length; i++)
+            {
+                array[top] = value[i];
+                top++;
+            }
+            Console.WriteLine($"{value}入栈");
+           
+            ArrayShow.Inline(array);
+            Console.WriteLine();
+            Console.WriteLine("------------------");
+            if (top == array.Length)
+            {
+                Console.WriteLine("栈溢出");
+            }
+        }
+        internal void pop()
+        {
+            Console.WriteLine($"{array[top - 1]}出栈");
+            array[top - 1] = 0;
+            top--;
+            ArrayShow.Inline(array);
+            Console.WriteLine();
+            Console.WriteLine("------------------");
+            if (top == array.Length)
+            {
+                Console.WriteLine("栈溢出");
+            }
+            else if (top == 0)
+            {
+                Console.WriteLine("栈已空");
+            }
+        }
 
+    }
+    class ArrayShow
+    {
+        internal static void Inline(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
 
-
-
-            Console.Read();
+                Console.Write(array[i] + "   ");
+            }
         }
     }
 }
