4770757f08103cac6f94e9e20ebfe1292bec39e2
自由之翼 <@LAPTOP-0QVJ42G4>
2019-04-18 22:39:40 +0800

    C#,接口and,javascript

---
+++ assignment/Interface.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace assignment
+{
+    public  class Eat
+    {
+        public  void Lunch()
+        {
+            Console.WriteLine("吃午饭");
+        }
+    }
+    interface Sleep
+    {
+        void Rest();
+    }
+    interface Bathe
+    {
+        void Swim();
+    }
+    public class Man: Eat, Sleep, Bathe
+    {
+        public void Rest()
+        {
+            Console.WriteLine("休息");
+        }
+        public void Swim()
+        {
+            Console.WriteLine("游泳");
+        }
+    }
+}
