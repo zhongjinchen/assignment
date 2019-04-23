1f2c94baa2c4147a2b9386c69a7492ab74b75d6d
自由之翼 <@LAPTOP-0QVJ42G4>
2019-04-23 22:11:41 +0800

    javascipt,闭包

---
+++ htmlCssBookStrap/wwwroot/js/site.js
@@ -0,0 +1,37 @@
+﻿//function student() {
+//    var name="xiaolong", age=20, female="ture";
+//    return function () {
+//        return name+ age+ female;
+//    }
+//}
+//var classmate = student();
+
+//console.log(classmate());
+
+//function foo(x) {
+//    var tmp = 3;
+//    return function (y) {
+//        x = x ? x + 1 : 1;
+//        console.log(x + y + tmp);
+//    }
+//}
+
+//var bar = foo(-1);
+//bar(10);
+
+function buildList(list) {
+    var result = [];
+    for (var i = 0; i < list.length; i++) {
+        result.push(function () {
+            console.log('item' + i + ': ' + list[i])
+        });
+    }
+    return result;
+}
+
+(function () {
+    var fnlist = buildList([1, 2, 3]);
+    for (var i = 0; i < fnlist.length; i++) {
+        fnlist[i]();
+    }
+})();
