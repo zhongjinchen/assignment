05a289981253b0a2c0e1fe49a80f296c7f2a9e98
自由之翼 <@LAPTOP-0QVJ42G4>
2019-06-25 22:15:50 +0800

    Task

--- Practise1/Program.cs
+++ Practise1/Program.cs
@@ -120,43 +120,185 @@ namespace Practise
             //Console.WriteLine("111");
             //Console.WriteLine("111");
 
-            Action getup=() =>
+            Action getup = () =>
+              {
+                  Console.WriteLine($"Action里面的1：Task-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+                  Console.WriteLine($"Action里面的2：Task-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+                  
+              };
+
+            //Func<int,long> func = (x) => {
+            //    Console.WriteLine($"Func里面的1：Task-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+            //    Console.WriteLine($"Func里面的2：Task-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+
+            //    return DateTime.Now.Ticks;
+            //};
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine($"第{i + 1}次");
+            //    //Task t = new Task(getup);
+            //    Task<long> t = new Task<long>((x) =>
+            //    {
+            //        Console.WriteLine($"Func里面的1：Task-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+            //        Console.WriteLine($"Func里面的2：Task-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+
+            //        return DateTime.Now.Ticks;
+            //    }, 2);
+            //    t.Start();
+
+            //    //Console.WriteLine($"外面的：Task-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+            //    //Console.WriteLine("wwwwwwwwwwwwwwwwwwwwwwww");
+            //    //Console.WriteLine("dddddddddddddddddddddddddddddd");
+            //    //Console.WriteLine("sssssssssssssssssssssssssss");
+
+            //    //t.RunSynchronously();       //同步运行
+            //    //t.Wait();                   //等待这个任务完成
+            //    //Task t = Task.Run(getup);
+            //    //Task t = Task.Factory.StartNew(getup);
+            //    //Console.WriteLine($"t.result={t.Result}");
+            //    //Console.WriteLine("呦吼");
+            //    //Console.WriteLine();
+
+            //    //继续
+            //    //t.ContinueWith((x) => { });
+            //}
+            //异常处理
+            //Task[] getups = new Task[10];
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    getups[i] = Task.Factory.StartNew(x =>  throw new Exception(x.ToString()),i);
+
+            //}
+            //try
+            //{
+            //    Task.WaitAll(getups);
+            //}
+            //catch (AggregateException ea)
+            //{
+
+            //    foreach (var item in ea.InnerExceptions)
+            //    {
+            //        Console.WriteLine(item.Message);
+            //    }
+            //另一种方法
+            //    //    ea.Handle((x) => {
+            //    //        Console.WriteLine(x.Message);
+            //    //        return true;
+            //    //    });
+
+            //}
+
+            //Task c= Task.Run(()=> {
+
+            //    Thread.Sleep(30);
+            //    throw new Exception("异常");
+            //});
+            //while (!c.IsCompleted)
+            //{
+
+            //    Console.WriteLine(c.Status);
+            //}
+            //Console.WriteLine(c.IsFaulted);
+
+            //if (c.IsFaulted)
+            //{
+            //    foreach (var item in c.Exception.InnerExceptions)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //}
+
+            ////取消
+            //CancellationTokenSource source = new CancellationTokenSource();
+            //CancellationToken token = source.Token;
+            //Task tokTask = Task.Run(() =>
+            //{
+
+            //    Console.WriteLine("取消前");
+            //    //token.ThrowIfCancellationRequested();
+            //    token.Register(() => {
+            //        Console.WriteLine("token.Register");
+            //        throw new Exception();
+            //    });
+            //    Console.WriteLine("取消后");
+            //}, token);
+
+            //Thread.Sleep(1);
+            //source.Cancel();
+
+            //try
+            //{
+            //    tokTask.Wait();            //一定要用wait才能捕获到线程上面的异常
+            //}
+            //catch (AggregateException e)
+            //{
+            //    throw;
+            //}
+
+            //异步方法
+            //GetTaskAsync();
+            //wrap();
+            //Console.WriteLine($"1，外面TaskId-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+            //Console.WriteLine($"2，外面TaskId-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+
+            //基于Task的并行
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine($"Action里面的1：Task-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
-                Console.WriteLine($"Action里面的2：Task-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
-               
-            };
-               
-            Func<long> func = () => {
-                Console.WriteLine($"Func里面的1：Task-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
-                Console.WriteLine($"Func里面的2：Task-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
-
-                return DateTime.Now.Ticks;
-            };
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine($"第{i + 1}次");
-                //Task t = new Task(getup);
-                Task<long> t = new Task<long>(func);
-                t.Start();
-
-                Console.WriteLine($"外面的：Task-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
-                Console.WriteLine("wwwwwwwwwwwwwwwwwwwwwwww");
-                Console.WriteLine("dddddddddddddddddddddddddddddd");
-                Console.WriteLine("sssssssssssssssssssssssssss");
-
-                //t.RunSynchronously();       //同步运行
-                t.Wait();
-                //Task t = Task.Run(getup);
-                //Task t = Task.Factory.StartNew(getup);
-                //Console.WriteLine($"t.result={t.Result}");
-                Console.WriteLine("呦吼");
-                Console.WriteLine();
-
-
+                Parallel.Invoke(() => {
+                    Console.WriteLine($"A1TaskId-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+                    Console.WriteLine($"A2TaskId-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+                },()=> {
+                    Console.WriteLine($"B1TaskId-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+                    Console.WriteLine($"B2TaskId-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+                });
             }
+
+            //Task.Delay(1);
+            //Task.FromResult();
+
             Console.ReadLine();
         }
+
+        public static async Task<int> GetReusltAsync()
+        {
+            int result = await Task.Run(()=> {
+                Console.WriteLine($"1,result内TaskId-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine($"2,result内TaskId-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+
+                Console.WriteLine($"3,result内TaskId-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+                return new Random().Next();
+            });
+            return result;
+        }
+
+        public static async Task wrap()
+        {
+            Task<int> task = GetReusltAsync();
+            Console.WriteLine($"1,wrap内TaskId-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"2,wrap内TaskId-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+            int result = await task;
+            Console.WriteLine($"wrap内await之后result-{result},TaskId-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+
+        }
+
+
+        public static async Task GetTaskAsync()
+        {
+            Console.WriteLine($"1，async内await前TaskId-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"2，async内await前TaskId-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+
+            await Task.Run(() => {
+                Console.WriteLine($"1，await内TaskId-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine($"2，await内TaskId-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine($"3，await内TaskId-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+
+            });
+            Console.WriteLine($"1，async内await后TaskId-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"2，async内await后TaskId-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"3，async内await后TaskId-{Task.CurrentId},ThreadId-{Thread.CurrentThread.ManagedThreadId}");
+
+        }
+
         static void process()
         {
             Thread current = Thread.CurrentThread;
@@ -168,6 +310,6 @@ namespace Practise
             Console.WriteLine(Thread.GetDomain().FriendlyName);
             Console.WriteLine(current.ThreadState);
         }
-       
+
     }
 }
