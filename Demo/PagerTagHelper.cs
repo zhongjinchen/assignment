b21a303c050d70bdcd91b8d597f62df780cc3f19
自由之翼 <@LAPTOP-0QVJ42G4>
2019-07-06 22:49:49 +0800

    [ASP.NET-1]作业

---
+++ Demo/PagerTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    [HtmlTargetElement("pager",Attributes ="pageIndex,path")]
+    public class PagerTagHelper:TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            object pageIndex = context.AllAttributes["pageIndex"].Value;
+            object path = context.AllAttributes["path"].Value;
+            output.Attributes.Add("href",$"{path}/Page-{pageIndex}");
+        }
+    }
+}
