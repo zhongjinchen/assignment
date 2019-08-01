7d778722a8718859ef0f7c7f4ac35b4a9bb6a55c
自由之翼 <@LAPTOP-0QVJ42G4>
2019-08-01 22:26:08 +0800

    FrameWork练习

---
+++ UI/Controllers/RegisterController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UI.Models.Register;
+
+namespace UI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(IndexModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return View();
+        }
+    }
+}
