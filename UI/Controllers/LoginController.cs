11af39e42afdc118347dc196ecf892b38466cd0a
自由之翼 <@LAPTOP-0QVJ42G4>
2019-08-15 22:15:16 +0800

    退出登录

--- UI/Controllers/LoginController.cs
+++ UI/Controllers/LoginController.cs
@@ -51,7 +51,6 @@ namespace UI.Controllers
                 return View(model);
             }
 
-
             //Response.Cookies[Const.COOKIE_USER][ConstRep.COOKIE_USERNAME] = user.UserName;
             //Response.Cookies[ConstRep.COOKIE_USER][ConstRep.COOKIE_PASSWORD] = user.Password;
             //Response.Cookies[ConstRep.COOKIE_USER].Expires = DateTime.Now.Add(new TimeSpan(7, 0, 0, 0, 0));
@@ -74,6 +73,23 @@ namespace UI.Controllers
                });
 
             return View(model);
+        }
+
+        public ActionResult LoginOut()
+        {
+            HttpCookie User_IdCookie = new HttpCookie(Const.USER_ID);
+            User_IdCookie.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(User_IdCookie);
+
+            HttpCookie User_NameCookie = new HttpCookie(Const.USER_NAME);
+            User_NameCookie.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(User_NameCookie);
+
+            HttpCookie User_PasswordCookie = new HttpCookie(Const.USER_PASSWORD);
+            User_PasswordCookie.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(User_PasswordCookie);
+
+            return Redirect(Request.Params["currentUser"]);
         }
     }
 }
